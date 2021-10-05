4e465683b6a3cf2a13b8ef1c4b2e388030d41020
Pedro de Castilho <pedrodecastilho@protonmail.com>
2021-10-05 15:09:10 -0300

    Parte 3

--- script.js
+++ script.js
@@ -16,4 +16,19 @@ const shuffleOrder = () => {
   const colorOrder = Math.floor(Math.random() * 4);
   order[order.length] = colorOrder;
   clickedOrder = [];
+
+  for (let i in order) {
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
+  }
+};
+
+let lightColor = (element, number) => {
+  number = number * 500;
+  setTimeout(() => {
+    element.classList.add("selected");
+  }, number - 250);
+  setTimeout(() => {
+    element.classList.remove("selected");
+  });
 };
